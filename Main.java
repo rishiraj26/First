c4942abfaea80a77e80f08a5bd154a636c9f3e2b
Rishi Raj <rjrishi@amazon.com>
2019-06-29 18:58:43 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        int j=2;
+        for(int i=0;i<=10;i++)
+        {
+            j*=2;
+        }
+        System.out.println(j);
+	// write your code here
+    }
+}
